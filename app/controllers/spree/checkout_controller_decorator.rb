1c481b85ab969873aa9809c5d203cf6cebbf21a0
Michael Runyon <mrunyon@ibsinternet.com>
2017-03-20 12:26:36 -0400

    attempting conversion of key value to integer instead

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -6,11 +6,14 @@ module Spree
 
       def sufficient_loyalty_points
         payment_method_ids = params[:order][:payments_attributes].collect do |payment|
+	  payment.each do |key, value|
+		  payment[key.to_i]
+	  end
           #payment["payment_method_id"]
           #payment[:payment_method_id]
-	  payment.each do |key, value|
-		  payment[key][:payment_method_id].to_i
-	  end
+	  #payment.each do |key, value|
+		  #payment[key][:payment_method_id].to_i
+	  #end
         end
         if Spree::PaymentMethod.loyalty_points_id_included?(payment_method_ids) && !@order.user.has_sufficient_loyalty_points?(@order)
           flash[:error] = Spree.t(:insufficient_loyalty_points)
